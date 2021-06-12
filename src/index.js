e581b35409cd9bb2a7a1128b0fae5dddeefa88eb
lukehaf <Luke.D.Hafermann.25@Dartmouth.edu>
2021-06-12 15:19:01 -0700

    ayup here's starterpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('starting up!');
+import $ from 'jquery';
+// change require to es6 import style
+
+import './style.scss';
+
+setInterval(display, 1000);
+
+let x = 2;
+
+function display() {
+  $('#main').html(`You've been on this page for ${x} seconds, my guy.`);
+  x++;
+}
