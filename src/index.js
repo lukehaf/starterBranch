1861e8822478925707d2c47e43c835566d71bfce
lukehaf <Luke.D.Hafermann.25@Dartmouth.edu>
2021-06-12 19:00:41 -0700

    test netlify

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './style.scss';
 
 let x = 2;
 function display() {
-  $('#main').html(`You've been on this page for ${x} seconds, my guy.`);
+  $('#main').html(`You've been on this page for ${x} seconds! Keep it up, go for a record...`);
   x += 1;
 }
 setInterval(display, 1000);
